24ae76bac37eb5c529f8d3b82d232fa8d2b4ff18
SWAPNIL ARGADE <swapnilargade@SWAPNILs-MacBook-Air.local>
2024-12-15 21:17:33 +0530

    Commit my code

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+const name = "Swapnil";
+const num = 7;
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +14,21 @@ function App() {
           {/* Edit <code>src/App.js</code> and save to reload. */}
           Bayerische Motoren Werke
         </p>
+        <h1>ASRS</h1>
+        <ul>
+          <li>Automatic Storage And Retrival System</li>
+          <li>Extended Warehouse Management System</li>
+          <li>Automatic Guided Vehicles</li>
+          <li>Rail Guided Vehicles</li>
+          <li>Mother Baby</li>
+          <li>Lifter</li>
+          <li>Stacker</li>
+          <li>Roller Conveyor</li>
+          <li>Chain Conveyor</li>
+          <li>Sizzer Lift</li>
+          <h2>Hello{name}</h2>
+          <p>My lucky num is {num}</p>
+        </ul>
         <a
           className="App-link"
           // href="https://reactjs.org"
@@ -18,10 +37,15 @@ function App() {
           rel="noopener noreferrer"
         >
           Visit 
+ 
         </a>
+        
       </header>
+
     </div>
   );
 }
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -15,3 +15,8 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+//  var React = require("react");
+// var ReactDOM = require("react-dom");
+
+// ReactDOM.render(<h1>Hello React!</h1>, document.getElementById("root"));
